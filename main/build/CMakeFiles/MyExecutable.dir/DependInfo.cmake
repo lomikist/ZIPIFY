ae6a62d388fcbe17c721c795cb631aaea3e961af
arman <armansarg01@gmail.com>
2024-07-01 22:23:05 +0400

    added: many things ))

---
+++ main/build/CMakeFiles/MyExecutable.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/weasle/repos/ZIPIFY/main/src/bitWriter.cpp" "CMakeFiles/MyExecutable.dir/src/bitWriter.cpp.o" "gcc" "CMakeFiles/MyExecutable.dir/src/bitWriter.cpp.o.d"
+  "/home/weasle/repos/ZIPIFY/main/src/fileData.cpp" "CMakeFiles/MyExecutable.dir/src/fileData.cpp.o" "gcc" "CMakeFiles/MyExecutable.dir/src/fileData.cpp.o.d"
+  "/home/weasle/repos/ZIPIFY/main/src/main.cpp" "CMakeFiles/MyExecutable.dir/src/main.cpp.o" "gcc" "CMakeFiles/MyExecutable.dir/src/main.cpp.o.d"
+  "/home/weasle/repos/ZIPIFY/main/src/unzipify.cpp" "CMakeFiles/MyExecutable.dir/src/unzipify.cpp.o" "gcc" "CMakeFiles/MyExecutable.dir/src/unzipify.cpp.o.d"
+  "/home/weasle/repos/ZIPIFY/main/src/zipify.cpp" "CMakeFiles/MyExecutable.dir/src/zipify.cpp.o" "gcc" "CMakeFiles/MyExecutable.dir/src/zipify.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ main/build/CMakeFiles/MyExecutable.dir/src/net.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/MyExecutable.dir/src/net.cpp.o: \
+ /home/weasle/repos/ZIPIFY/main/src/net.cpp /usr/include/stdc-predef.h
