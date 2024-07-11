6c6fde8369e672c68c95a51981dbb19e8e944c1f
arman <armansarg01@gmail.com>
2024-07-11 15:58:20 +0400

    removed qt folder

---
+++ test/build/CMakeFiles/BoostGILExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BoostGILExample"
+  "BoostGILExample.pdb"
+  "CMakeFiles/BoostGILExample.dir/main.cpp.o"
+  "CMakeFiles/BoostGILExample.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BoostGILExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
